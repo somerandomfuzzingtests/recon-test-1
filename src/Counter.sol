57a8e08d53013a680f8328d313479028dc7e0916
test <test@example.com>
2024-04-01 07:51:03 +0200

    feat: functionality

--- src/Counter.sol
+++ src/Counter.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public number;
+    uint64 public number;
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
